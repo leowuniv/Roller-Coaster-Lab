e81073c553cc7f695c446bb157d463b7376626d6
Leo <leowuniv@gmail.com>
2025-03-17 02:05:00 -0700

    Update RollerCoasterLines.py

--- RollerCoasterLines.py
+++ RollerCoasterLines.py
@@ -1,5 +1,42 @@
+#  Stack – Elevator Drop Ride Simulation
+
 class Stack:
   def __init__():
 
+class ElevatorRide:
+'''
+This class uses a stack to simulate guests entering an elevator ride where each guests boards one at a time and exit in reverse order (the last person in is the first out).
+'''
+
+# method board_guest(guest_name)
+
+# method start_ride(capacity)
+
+# ========================================================
+
+# Queue – Roller Coaster Ride Simulation
+
 class Queue: 
   def __init__():
+
+class RollerCoasterRide:
+'''
+This class uses a queue to manage guests waiting for the roller coaster where guests enter the queue and exit in the same order they arrived (first in, first out).
+'''
+
+# method join_queue(guest_name)
+
+# method start_ride(capacity)
+
+# ========================================================
+
+# Priority Queue – VIP Guest Management
+
+class VIPRide:
+'''
+This class uses a priority queue to manage guests based on priority levels (e.g., VIP or Fast Pass holders) or where guests with higher priority board the ride before others.
+'''
+
+# method add_guest(guest_name, priority)
+
+# method start_ride(capacity)
