882d0ab409cc87a4fdbc71c2284410a7f959891a
Leo <leowuniv@gmail.com>
2025-03-17 23:35:04 -0700

    Test function for VIP Ride

--- RollerCoasterLines.py
+++ RollerCoasterLines.py
@@ -1,33 +1,3 @@
-#  Stack – Elevator Drop Ride Simulation
-
-class Stack:
-  def __init__():
-
-class ElevatorRide:
-'''
-This class uses a stack to simulate guests entering an elevator ride where each guests boards one at a time and exit in reverse order (the last person in is the first out).
-'''
-
-# method board_guest(guest_name)
-
-# method start_ride(capacity)
-
-# ========================================================
-
-# Queue – Roller Coaster Ride Simulation
-
-class Queue: 
-  def __init__():
-
-class RollerCoasterRide:
-'''
-This class uses a queue to manage guests waiting for the roller coaster where guests enter the queue and exit in the same order they arrived (first in, first out).
-'''
-
-# method join_queue(guest_name)
-
-# method start_ride(capacity)
-
 # ========================================================
 
 # Priority Queue – VIP Guest Management
@@ -40,3 +10,14 @@ This class uses a priority queue to manage guests based on priority levels (e.g.
 # method add_guest(guest_name, priority)
 
 # method start_ride(capacity)
+
+# ========================================================
+
+#test for vip ride (priority q)
+def main():
+    #test for no people in ride
+    rideED = VIPRide()
+    
+    
+if __name__ == "__main__":
+    main()
